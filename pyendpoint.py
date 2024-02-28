ef7b131049b64484327871af30271b482f767b56
jeradkinggeo <JeradKingGeo@gmail.com>
2024-02-28 14:43:24 -0600

    stuff

--- pyendpoint.py
+++ pyendpoint.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Dict
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"])
 
 class GeoJSON(BaseModel):
     type: str
